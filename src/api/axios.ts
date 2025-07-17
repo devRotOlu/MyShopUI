7ce91c268f8ca3480efcea062fe71f0e7ba2d74e
Olumide Rotimi <rotimiolumide68@gmail.com>
2025-07-17 09:48:36 +0100

    Corected base URL

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -9,7 +9,7 @@ let isRefreshing: boolean = false;
 let failedRequests: failedRequestType[] = [];
 
 export const myShopAxios = axios.create({
-  baseURL: "https://myshopapi-ggcb.onrender.com",
+  baseURL: "https://myshopapi-ggcb.onrender.com/",
   headers: {
     "Content-Type": "application/json",
     "X-Client-Type": "web",
